8f73ebea2cb69dd2918f1bdbaef88fb941fd0412
adampodolak <podola2@mcmaster.ca>
2025-06-05 15:15:53 -0400

    Added backend API with SQL integration for Employee table entity. Created DB using SQL Express and SQL Server Management Studio on localhost. Configured Entity Framework (EF) core db context and connected to sql server. Created Employee contoller with full create, read, update, delete (CRUD) operations. Connected backend to db via connection string in appsettings.json.

---
+++ src/WorkforceAnalyticsDashboard/WorkforceAnalyticsDashboard/Models/Employee.cs
@@ -0,0 +1,13 @@
+public class Employee
+{
+    public int EmployeeID { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTime HireDate { get; set; }
+    public int DepartmentID { get; set; }
+    public int JobID { get; set; }
+    public decimal Salary { get; set; }
+    public string? Status { get; set; }
+}
